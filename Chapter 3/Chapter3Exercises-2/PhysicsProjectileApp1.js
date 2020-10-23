30888247b4f896661ddc06ebc1a32123e489d8bc
grinsteada <72751430+grinsteada@users.noreply.github.com>
2020-10-23 19:36:07 -0400

    Add files via upload

---
+++ Chapter 3/Chapter3Exercises-2/PhysicsProjectileApp1.js
@@ -0,0 +1,11 @@
+function display() {
+    var gallons = document.getElementById("gallons").value;
+    // var velocity = document.getElementById(
+    //   "velocity").value;
+  
+    document.getElementById("gallonsDisplay").innerHTML =
+      gallons;
+  
+    // document.getElementById("velocityDisplay").innerHTML =
+    //   velocity;
+  }
